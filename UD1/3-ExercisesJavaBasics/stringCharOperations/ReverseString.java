5a5e4f1b1a0038c657225be9fcb9ca561db4269b
Gorka Garcia Altube <garcia.gorka@uni.lan>
2022-10-06 14:18:29 +0200

    06/10/2022-14:18

---
+++ UD1/3-ExercisesJavaBasics/stringCharOperations/ReverseString.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        String inStr;
+        int inStrLen;
+        int cont = 1;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Sartu nahi duzun hitza: ");
+        inStr = in.next();
+        inStrLen = inStr.length();
+        in.close();
+        int index = inStrLen - 1;
+
+        System.out.print("Sartu duzun hitzaren alderantziskoa hau da: ");
+        do {
+            System.out.print(inStr.charAt(index));
+            --index;
+            ++cont;
+        } while(cont <= inStrLen);
+
+    }
+}
