d1b1d47a384907c911510f67b909b8ee4a32f225
Gorka Garcia Altube <garcia.gorka@uni.lan>
2022-10-07 09:55:56 +0200

    07/10/2022-9:55

---
+++ UD1/3-ExercisesJavaBasics/stringCharOperations/CaesarsCode.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CaesarsCode {
+    public static void main(String[] args) {
+
+        String word;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Saru kodifikatzea nahi duzun hitza: ");
+        word = in.next().toUpperCase();
+        in.close();
+
+        
+
+    }
+}
