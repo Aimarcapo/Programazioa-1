18b6ac333e5edc6ce562a455057b7b37d3206b58
Gorka Garcia <garciagorka@uni.eus>
2022-11-24 18:12:34 +0100

    Partxisa

--- UD2/00-AriketaGehigarriak/src/random/Prog3.java
+++ UD2/00-AriketaGehigarriak/src/random/Prog3.java
@@ -4,62 +4,66 @@ import java.util.Scanner;
 
 public class Prog3 {
     public static void main(String[] args) throws InterruptedException {
+        int erantzuna = 0;
+        int[] totala = {0, 0};
+        char jarraitu;
+        boolean[] kanpoan = {false, false};
 
-        String erantzuna;
-        boolean kanpoan = false;
-        int kontua = 0;
-        int[] aurrekoTiradak;
-        int b = 0;
-
-        aurrekoTiradak = new int[100];
+        System.out.println("Jola honek, partxis moduko, 2 jokalarien arteko lehiaketa bat da. 35-era heltzen duen lehenengoa partida irabazten du.");
+        System.out.println("Aukeratu zein izango den lehenengo jokalaria eta zein bigarrena.");
+        for(int X = 0; X < 6; X++){
+            System.out.print(". ");
+            Thread.sleep(1000);
+        }
+        System.out.println("Jolastu nahi duzue (B/E)?");
         
-
         Scanner in = new Scanner(System.in);
+        jarraitu = in.next().charAt(0);
+        System.out.println("");
 
-        do{
-            System.out.print("Dadoa bota nahi duzu (bai/ez)? ");
-            erantzuna = in.next();
-            System.out.println("");
-            if(erantzuna.equals("ez")){
+        /*JOLASA HASTEKO/JARRAITZEKO */
+        while (jarraitu == 'B'){
+
+            /*JOKALARI BAKOITZAREN TURNOARENTZAKO */
+            for (int i = 1; i <= 2; i++) {
+                System.out.println(i + ". jokalariaren turnoa, BOTA DADOA!");
+                for(int X = 0; X < 3; X++){
+                    System.out.print("clack ");
+                    Thread.sleep(500);
+                }
+
+                erantzuna = (int)(Math.random()*6+1);
+                System.out.print("=> " + erantzuna);
+
+                if(kanpoan[i-1] == true){
+                    totala[i-1] = totala[i-1] + erantzuna;
+                    System.out.println("=> " + i + ". jokalariaren posizioa: " + totala[i-1]);
+                }else{
+                    if(erantzuna == 5){
+                        System.out.println(" => Zorionak! fitxa atera duzu.");
+                        kanpoan[i-1] = true;
+                    } else{
+                        System.out.println(" => Ez duzu fitxa atera");
+                    }
+                }
+                if  (totala[i-1] >= 25){
+                    System.out.println("ZOIONAK!!");
+                    System.out.println(i + ". jokalariak irabazi du partida.");
+                    break;
+                }
+            }
+            if  (totala[0] >= 25 || totala[1] >= 25){
                 break;
             }
 
-            for(int i = 0; i < 5; i++){
-                System.out.print(". ");
-                Thread.sleep(500);
-            }
-            System.out.print("=> ");
-            int dadoa = (int)(Math.random()*6 +1);
-            System.out.print(dadoa + " =>");
-
-            if(kanpoan == true){
-                System.out.print(" " + dadoa + " kasilla aurreratu dituzu");
-                kontua += dadoa;
-                
-                /*666 ateratzen bada etxera bidaltzeko */    
-                /*            
-                aurrekoTiradak[b]=dadoa;
-                b++;
-                if(b >= 3 && ((aurrekoTiradak[b-2] == 6) && (aurrekoTiradak[b-1] == 6) && (aurrekoTiradak[b] == 6))){
-                    System.out.println("3 aldiz jarraian atera duzu 6, berez, etxera bueltatzen duzu.");
-                    kontua = 0;
-                    kanpoan = false;
-                */
-                }
-            }else{
-                if(dadoa == 5){
-                    System.out.print(" Zorionak! fitxa bat atera duzu.");
-                    kanpoan = true;
-                } else{
-                    System.out.print(" Txarto bez.");
-                }
-            }    
+            System.out.println("Berriz jolastu nahi duzue (B/E)?");
+            jarraitu = in.next().charAt(0);
             System.out.println("");
+        }
 
-        }while(erantzuna.equals("bai"));
-
-        System.out.println(kontua + " kasilla aurreratu dituzu guztira.");
-
+        System.out.println("");
+        System.out.println("Eskerrik asko jolasteagatik!!");
         in.close();
     }
+
 }
