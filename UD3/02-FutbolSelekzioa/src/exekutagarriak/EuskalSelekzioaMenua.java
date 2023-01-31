cc469e7b408265a81d26f953ef96db8b5ed40b51
Gorka Garcia <garcia.gorka@uni.eus>
2023-01-31 14:28:40 +0100

    31/01/2023

--- UD3/02-FutbolSelekzioa/src/exekutagarriak/EuskalSelekzioaMenua.java
+++ UD3/02-FutbolSelekzioa/src/exekutagarriak/EuskalSelekzioaMenua.java
@@ -1,11 +1,17 @@
 package exekutagarriak;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import model.Demarkazioa;
+import model.Entrenador;
+import model.Futbolista;
+import model.IntegranteSeleccion;
+import model.Masajista;
 
 public class EuskalSelekzioaMenua {
     public static void main(String[] args) {
-        boolean egia = true;
-        int aukera;
+        int aukera = 0;
 
         Scanner in = new Scanner(System.in);
 
@@ -24,56 +30,153 @@ public class EuskalSelekzioaMenua {
 
             switch(aukera){
                 case 1:
-                    System.out.println("");
                     datuakIkusi();
-                    System.out.println("");
-
+                    break;
                 case 2:
-                    System.out.println("");
-                    datuakTaldekaIkusi();
-                    System.out.println("");
-
+                    datuakIkusiTaldekatuta();
+                    break;
                 case 3:
-
-
+                    partaideakAlfabetikokiIkusi();
+                    break;
                 case 4: 
+                    aldaketa();
+                    break;
                 case 5:
+                    break;
                 case 6:
                 case 20:
+                    break;
                 default:
+                    System.out.println("Sartutako aukera ez da egokia...");
             }
 
-        } while(egia);
-        
+        } while(aukera != 20);
+        System.out.println("Eskerrik asko nire menua erabiltzeagatik!");
         in.close();
     }
 
     public static void datuakIkusi(){
         EuskalSelekzioa.selekzioOsoaSortu();
+        int countFut = 0, countEntr = 0, countMas = 0, countBest = 0;
+        System.out.println();
         for (int index = 0; index < EuskalSelekzioa.selekzioa.size(); index++) {
             System.out.println(EuskalSelekzioa.selekzioa.get(index));
+            if(EuskalSelekzioa.selekzioa.get(index).getClass() == Futbolista.class){
+                countFut++;
+            } else if(EuskalSelekzioa.selekzioa.get(index).getClass() == Entrenador.class){
+                countEntr++;
+            } else if(EuskalSelekzioa.selekzioa.get(index).getClass() == Masajista.class){
+                countMas++;
+            } else{
+                countBest++;
+            }
         }
-        //GUZTIRA KALKULATZEA FALTA DA.
+        System.out.println("GUZTIRA: " + countFut + " futbolari, " + countEntr + " entrenatzaile, " + countMas + " masajista, eta " + countBest + " bestelakoak" );
+        System.out.println("");
     }
 
-    public static void datuakTaldekaIkusi(){
+    public static void datuakIkusiTaldekatuta(){
         EuskalSelekzioa.selekzioOsoaSortu();
-        for (int index = 0; index < EuskalSelekzioa.selekzioa.size(); index++) {
-            if(index == 0){
-                System.out.println("");
-                System.out.println("FUTBOLARIAK\n==============");
-            } else if(index == 17){
-                System.out.println("");
-                System.out.println("ENTRENATZAILEAK\n==============");
-            }else if(index == 20){
-                System.out.println("");
-                System.out.println("MASAJISTAK\n==============");
-            } else if(index == 21){
-                System.out.println("");
-                System.out.println("BESTELAKOAK\n==============");
+        int count = 0;
+        ArrayList<IntegranteSeleccion> fut = new ArrayList<>();
+        ArrayList<IntegranteSeleccion> entre = new ArrayList<>();
+        ArrayList<IntegranteSeleccion> mas = new ArrayList<>();
+        ArrayList<IntegranteSeleccion> best = new ArrayList<>();
+
+        for (int i = 0; i < EuskalSelekzioa.selekzioa.size(); i++) {
+            if(EuskalSelekzioa.selekzioa.get(i).getClass() == Futbolista.class){
+                fut.add(EuskalSelekzioa.selekzioa.get(i));
+            } else if(EuskalSelekzioa.selekzioa.get(i).getClass() == Entrenador.class){
+                entre.add(EuskalSelekzioa.selekzioa.get(i));
+            } else if(EuskalSelekzioa.selekzioa.get(i).getClass() == Masajista.class){
+                mas.add(EuskalSelekzioa.selekzioa.get(i));
+            } else{
+                best.add(EuskalSelekzioa.selekzioa.get(i));
             }
-            System.out.println(EuskalSelekzioa.selekzioa.get(index));
         }
-        //GUZTIRA KALKULATZEA FALTA DA.
+        //FUTBOLARIAK
+        System.out.println("");
+        System.out.println("FUTBOLARIAK\n------------------");
+        for (int i = 0; i < fut.size(); i++) {
+            System.out.println(fut.get(i));
+            count++;
+        }
+        System.out.println("GUZTIRA: " + count);
+        count = 0;
+        //ENTRENADOREAK
+        System.out.println("");
+        System.out.println("ENTRENATZAILEAK\n------------------");
+        for (int i = 0; i < entre.size(); i++) {
+            System.out.println(entre.get(i));
+            count++;
+        }
+        System.out.println("GUZTIRA: " + count);
+        count = 0;
+        //MASAJISTAK
+        System.out.println("");
+        System.out.println("MASAJISTAK\n------------------");
+        for (int i = 0; i < mas.size(); i++) {
+            System.out.println(mas.get(i));
+            count++;
+        }
+        System.out.println("GUZTIRA: " + count);
+        count = 0;
+        //BESTELAKOAK
+        System.out.println("");
+        System.out.println("BESTELAKOAK\n------------------");
+        for (int i = 0; i < best.size(); i++) {
+            System.out.println(best.get(i));
+            count++;
+        }
+        System.out.println("GUZTIRA: " + count);
+        System.out.println("");
+    }
+    
+    //EGITEKE
+    public static void futbolariakAlfabetikokiIkusi(){
+
+    }
+
+    //EGITEKE
+    public static void partaideakAlfabetikokiIkusi(){
+    
+    }
+
+    public static void aldaketa(){
+        int id;
+        String postua;
+        EuskalSelekzioa.selekzioOsoaSortu();
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Zein jokalariaren posizioa aldatu nahi duzu (ID-a esan)?");
+        id = in.nextInt();
+
+        for (int i = 0; i < EuskalSelekzioa.selekzioa.size(); i++) {
+            if(EuskalSelekzioa.selekzioa.get(i).getId() == id){
+                System.out.println(((Futbolista)EuskalSelekzioa.selekzioa.get(i)).getDemarkazioa() + ", posizioan zengoen, zein posiziora aldatu nahi duzu (POR, DEF, MED, DEL)?");
+                postua = in.next();
+                switch(postua){
+                    case "POR":
+                        ((Futbolista)EuskalSelekzioa.selekzioa.get(i)).setDemarkazioa(Demarkazioa.POR);
+                        System.out.println("Ondo aldatu da.");
+                        break;
+                    case "DEF":
+                        ((Futbolista)EuskalSelekzioa.selekzioa.get(i)).setDemarkazioa(Demarkazioa.DEF);
+                        System.out.println("Ondo aldatu da.");
+                        break;
+                    case "MED":
+                        ((Futbolista)EuskalSelekzioa.selekzioa.get(i)).setDemarkazioa(Demarkazioa.MED);
+                        System.out.println("Ondo aldatu da.");
+                        break;
+                    case "DEL":
+                        ((Futbolista)EuskalSelekzioa.selekzioa.get(i)).setDemarkazioa(Demarkazioa.DEL);
+                        System.out.println("Ondo aldatu da.");
+                        break;
+                    default:
+                        System.out.println("Sartutako aukera ez da egokia.");
+                }
+            }
+        }
+        in.close();
     }
 }
