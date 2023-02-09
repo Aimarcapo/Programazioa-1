50bc26a16d9bdf0c51361974070eb19d23273367
Gorka Garcia <garcia.gorka@uni.eus>
2023-02-09 10:14:36 +0100

    09/02/2023.2

--- UD3/04-Erosketak/src/model/EpekakoErosketa.java
+++ UD3/04-Erosketak/src/model/EpekakoErosketa.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.ArrayList;
-
 public class EpekakoErosketa extends Erosketa {
     private int epeak = 12;
     private boolean ordainketakBukatua = false;
@@ -11,10 +9,29 @@ public class EpekakoErosketa extends Erosketa {
         return (kuota * epeak);
     }
     /*EGITEKE */
-    public EpekakoErosketa(String kodea, String data, Bezeroa bezeroa, ArrayList<Produktua> produktuak,
-            ArrayList<Integer> unitateak, double guztira, double kuota) {
-        super(kodea, data, bezeroa, produktuak, unitateak, guztira);
+    public EpekakoErosketa(String kodea, Bezeroa bezeroa, double guztira, double kuota) {
+        super(kodea, bezeroa, guztira);
         this.kuota = kuota;
     }
+    public int getEpeak() {
+        return epeak;
+    }
+    public void setEpeak(int epeak) {
+        this.epeak = epeak;
+    }
+    public boolean isOrdainketakBukatua() {
+        return ordainketakBukatua;
+    }
+    public void setOrdainketakBukatua(boolean ordainketakBukatua) {
+        this.ordainketakBukatua = ordainketakBukatua;
+    }
+    public double getKuota() {
+        return kuota;
+    }
+    public void setKuota(double kuota) {
+        this.kuota = kuota;
+    }
+
+    
     
 }
